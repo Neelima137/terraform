fb26a6d3a7289d4ac07c45cc4c9d08c42bb9877e
Neelima137 <devadineelima137@gmail.com>
2025-02-11 22:30:10 +0530

    Create variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id"{
+    type            = string
+    description     = "projectid"
+    default         = " << project name >>"
+}
+variable "region"{
+    type        = string
+    description = "region"
+    default     = "us-central1"
+}
+variable "name"{
+    type        = string
+    description = "vpc name "
+    default     = "tvpc"
+}
